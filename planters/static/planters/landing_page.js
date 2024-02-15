41deaffc5627634ab3e03b1450e2073213f22643
sanyamvatta <sanyamvatta4@gmail.com>
2024-02-15 08:50:55 +0530

    started nav

---
+++ planters/static/planters/landing_page.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    var navbarMenu = document.querySelector('.navbar-menu');
+    navbarMenu.classList.toggle('active');
+  });
