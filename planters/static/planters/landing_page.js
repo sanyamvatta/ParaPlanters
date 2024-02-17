879bcfbdae613fceb6afb7f8c112dec21d9c74a3
sanyamvatta <sanyamvatta4@gmail.com>
2024-02-17 08:46:17 +0530

    almost finish landing page

--- planters/static/planters/landing_page.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('mobile-menu').addEventListener('click', function() {
-    var navbarMenu = document.querySelector('.navbar-menu');
-    navbarMenu.classList.toggle('active');
-  });
