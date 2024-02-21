ddf18cb0894d1c47677c529da83419020ae005cf
sanyamvatta <sanyamvatta4@gmail.com>
2024-02-21 07:17:48 +0530

    finish about me section

--- planters/static/planters/landing/landing_page.js
+++ planters/static/planters/landing/landing_page.js
@@ -6,6 +6,12 @@ $('.navTrigger').click(function () {
 
 });
 
+AOS.init({
+  duration: 800, // values from 0 to 3000, with step 50ms
+  easing: 'ease-in-out', // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+});
 
 document.addEventListener("DOMContentLoaded", function() {
   const observer = new IntersectionObserver(entries => {
