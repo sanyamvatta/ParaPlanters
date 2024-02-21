58b88798fb309b5c1ead67b5161af43fb5ff6c37
sanyamvatta <sanyamvatta4@gmail.com>
2024-02-21 06:26:34 +0530

    finish cta section

--- planters/static/planters/landing/landing_page.js
+++ planters/static/planters/landing/landing_page.js
@@ -5,3 +5,28 @@ $('.navTrigger').click(function () {
   $("#mainListDiv").fadeIn();
 
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+
+        setTimeout(() => {
+          // Remove initial animation class if needed, e.g., 'animate'
+          ctaButton.classList.remove('animate');
+
+          // Add the class that triggers the infinite pulse animation
+          ctaButton.classList.add('button-pulse');
+      }, 3000); 
+      }
+    });
+  });
+
+  const ctaHeading = document.querySelector('.major-cta-heading');
+  const ctaButton = document.querySelector('.major-cta-button');
+  
+  observer.observe(ctaHeading);
+  observer.observe(ctaButton);
+});
